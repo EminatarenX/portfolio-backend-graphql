81f8a6676cf07e5bc6adea7864360059b6d239df
EminatarenX <eminataren2002@gmail.com>
2023-08-10 12:39:57 -0600

    anadiendo dotenv

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
-
+require('dotenv').config()
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
-server.listen().then(({ url }) => {
+server.listen({port: process.env.PORT || 4000}).then(({ url }) => {
     console.log(`Server ready at ${url}`)
 })
 
